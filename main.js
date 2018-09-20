a622a0a80149c27c994e92bca77d65ed3d7621e0
RD <rd@Suryas-MBP.attlocal.net>
2018-09-20 13:32:04 -0500

    basic node app:completed

---
+++ main.js
@@ -0,0 +1,21 @@
+const greetings = require("./greeting.js");
+
+// "Hello"
+greetings.greetingEnglish();
+
+// "Konichiwa"
+greetings.greetingJapanese();
+
+// "Hola"
+greetings.greetingSpanish();
+
+// "Hallo"
+greetings.greetingIcelandic();
+
+const maths = require("./math.js");
+
+// returns "40"
+maths.addVariables(10,30);
+
+//returns "10"
+maths.subVariables(20,10);
---
+++ math.js
@@ -0,0 +1,9 @@
+exports.addVariables = function(a,b) {
+  var sum = a + b;
+  console.log(sum);
+}
+
+exports.subVariables = function(a,b) {
+  var sub = a - b;
+  console.log(sub);
+}
